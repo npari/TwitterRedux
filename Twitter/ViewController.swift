af84a1d150a2b7f3a8f824a59842b17a488848e5
Nithya Pari <Nithya_Pari@intuit.com>
2017-04-10 07:29:38 -0700

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Pari, Nithya on 4/10/17.
+//  Copyright © 2017 Pari, Nithya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
